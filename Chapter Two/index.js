9f6837bfa542e4ca7dc7aac385e8c0dafa32d07c
React Dev <44882127+Arjunroy97@users.noreply.github.com>
2022-01-05 14:51:08 +0600

    Chapter two

---
+++ Chapter Two/index.js
@@ -0,0 +1,132 @@
+/**  Hello world and code staucture */
+
+// console.log("Hello world")
+
+
+/** What is variable in javaScript */
+
+ var funcScopeVariable = 10; // Function scope
+ let blockScopeVariable = 10; // Block scope
+ const blockScopeAndConstantVariable = 10; // Block scope
+
+
+
+ /** Reserved Word */
+
+//  abstract	arguments	await*	boolean
+// break	byte	case	catch
+// char	class*	const	continue
+// debugger	default	delete	do
+// double	else	enum*	eval
+// export*	extends*	false	final
+// finally	float	for	function
+// goto	if	implements	import*
+// in	instanceof	int	interface
+// let*	long	native	new
+// null	package	private	protected
+// public	return	short	static
+// super*	switch	synchronized	this
+// throw	throws	transient	true
+// try	typeof	var	void
+// volatile	while	with	yield
+
+
+/** Premitive Data Type in JavaScript */
+
+
+// Number Data Type e.a: 12, 1.2
+
+let number = 152;
+let flatNumber = 12.32;
+// console.log(typeof number)
+
+
+
+// String Data Type e.x: 'String' 
+
+let str = "I'm String Data Type"
+// console.log(str)
+
+
+
+// Boolean Data Type e.x: true,false
+
+let boolean = true;
+let booleans = false;
+// console.log(boolean, booleans)
+
+
+
+/**  Null & undefind */
+
+let ull = null; // This main Not a Number
+let undefindd = undefined;
+
+
+/** Arthmatic Operator */
+
+/** 
+ * +
+ * -
+ * *
+ * /
+ * %
+ * a++ = Decrement 
+ * ++a = Increment 
+ * --a = Increment 
+ * a-- = Decrement 
+ * && (AND)
+ * || (OR)
+ * = (Assinment)
+ */
+
+let a = 15;
+let b = 5;
+
+// Addition Operator
+let add = a + b;
+
+// subtruction
+let sub = a - b;
+
+// multipletion
+let mul = a * b;
+
+// Divition
+let div = a / b;
+
+// Modulas
+let mod = a % b;
+
+// a++ Decrement 
+let d = 5;
+d++ //5
+// console.log(d++)
+
+// ++a Increment 
+++d // 6
+
+
+/** Statement in javaScirpt */
+
+let sta = 'Hi i am Statement';
+let addOperation = 21 + (5-2) * 2;
+
+if(true){};
+function fuc(){};
+
+/** Math function provide by JavaScript */
+
+Math.floor(3.8); // 3
+Math.ceil(3.5) // 4
+Math.round(3.8) // 4
+Math.random(5.2)
+Math.pow(2, 2); // 8
+Math.sqrt(3);  // 9
+Math.abs(-1.3) // 1.3
+
+
+/** JavaScript Comment */
+
+// Singel line comment
+/** Multi line comment */ 
